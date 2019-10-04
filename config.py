8fd5bedf232f35228edcad507e290f7a49243851
Rodrigo Curiel <rodrigocuriel95@gmail.com>
2019-10-04 16:21:23 -0700

    preparing for heroku

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    'postgresql+psycopg2://username:password@localhost/champion'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or None
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or None
     POSTS_PER_PAGE = 25
