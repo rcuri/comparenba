9c0546be78aca92bd7aa9918db6ed219b60ec49e
Rodrigo Curiel <rodrigocuriel95@gmail.com>
2019-10-04 22:30:57 -0700

    updated heroku configurations

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'SECRET_KEY'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or None
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ELASTICSEARCH_URL = 'https://1pth9505od:59sv268usq@ivy-346702352.us-east-1.bonsaisearch.net:443'
-    #os.environ.get('ELASTICSEARCH_URL') or None
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or None
     POSTS_PER_PAGE = 25
