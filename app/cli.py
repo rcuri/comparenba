91c9daf5c65774d428fb09f237a1929be4ec5a27
Rodrigo Curiel <rodrigocuriel95@gmail.com>
2019-10-04 21:33:56 -0700

    removed unused dependencies

--- app/cli.py
+++ app/cli.py
@@ -1,8 +1,6 @@
 import os
 import click
 from app import db
-#from app.populate_players import populate_database
-from app.player_images import populate_images
 from app.populate_players_again import populate_database
 
 def register(app):
@@ -18,12 +16,4 @@ def register(app):
         except Exception as e:
             print("An error occurred.")
             print(e)
-            db.session.rollback()
-
-    @populate.command()
-    def images():
-        try:
-            populate_images(db)
-        except:
-            print("An error occurred with the images.")
             db.session.rollback()
